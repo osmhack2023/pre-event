ae5aeb2b2359f8de9e961fc4fdacc1b91ab7c6d2
Suyog Shrestha <suyogshrestha97@gmail.com>
2023-05-24 18:36:25 +0545

    timer

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 3000,
+  }
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -11,3 +14,33 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// timer
+let countDownDate = newDate("May 29, 2023 00:00").getTime();
+let x = setInterval(function () {
+  let now = newDate().getTime();
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  console.log(days);
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  console.log(hours);
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  console.log(minutes);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hr").innerHTML = hours;
+  document.getElementById("min").innerHTML = minutes;
+
+  if (distance > 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hr").innerHTML = "00";
+    document.getElementById("min").innerHTML = "00";
+  }
+
+}, 1000);
+
+
+
+
