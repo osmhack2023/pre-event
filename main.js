41f1b3ae37d02165c2eee2e797ba2b8e159f358c
Suyog Shrestha <suyogshrestha97@gmail.com>
2023-05-30 16:16:46 +0545

    done

--- main.js
+++ main.js
@@ -19,14 +19,12 @@
 let days = document.getElementById('days');
 let hours = document.getElementById('hours');
 let minutes = document.getElementById('minutes');
+let second = document.getElementById('second');
 
 let dd = document.getElementById('dd');
 let hh = document.getElementById('hh');
 let mm = document.getElementById('mm');
-
-let day_dot = document.querySelector('.day_dot');
-let hr_dot = document.querySelector('.hr_dot');
-let min_dot = document.querySelector('.min_dot');
+let ss = document.getElementById('ss');
 
 let endDate = 'July 12 2023 00:00:00';
 // date format mm/dd/yyyy
@@ -40,24 +38,19 @@ let x = setInterval(function () {
   let d = Math.floor(distance / (1000 * 60 * 60 * 24));
   let h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let s = Math.floor((distance % (1000 * 60)) / (1000));
 
   // output
   days.innerHTML = d + "<br><span>Days<span>";
   hours.innerHTML = h + "<br><span>Hours<span>";
-  minutes.innerHTML = m + "<br><span>Min<span>";
+  minutes.innerHTML = m + "<br><span>Minutes<span>";
+  second.innerHTML = s + "<br><span>Seconds<span>";
 
   // animation stroke
   dd.style.strokeDashoffset = 440 - (440 * d) / 365;
   hh.style.strokeDashoffset = 440 - (440 * h) / 24;
   mm.style.strokeDashoffset = 440 - (440 * m) / 60;
-
-  // animate circle dots
-  day_dot.style.transform = `rotateZ(${d * 0.986}deg)`;
-  //360deg/365days = 0.986
-  hr_dot.style.transform = `rotateZ(${h * 15}deg)`;
-  //360deg/24hr = 15
-  min_dot.style.transform = `rotateZ(${m * 6}deg)`;
-  //360deg/60min = 6
+  ss.style.strokeDashoffset = 440 - (440 * s) / 60;
 
   // if countdown is over deadline
   if (distance < 0) {
@@ -65,4 +58,27 @@ let x = setInterval(function () {
     document.getElementById("time").style.display = 'none';
     document.querySelector(".end").style.display = 'block';
   }
+
+  let hrString = hr;
+  let minString = min;
+  let secString = sec;
+  let dayString = day;
+
+  if (day < 10) {
+    dayString = "0" + dayString;
+  }
+  if (min < 10) {
+    minString = "0" + minString;
+  }
+  if (sec < 10) {
+    secString = "0" + secString;
+  }
+  if (hr < 10) {
+    hrString = "0" + hrString;
+  }
+
+  document.getElementById("days").innerHTML = dayString;
+  document.getElementById("hours").innerHTML = hrString;
+  document.getElementById("minutes").innerHTML = minString;
+  document.getElementById("second").innerHTML = secString;
 }, 1000)
